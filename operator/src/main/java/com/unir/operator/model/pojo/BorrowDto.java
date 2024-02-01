5522529a863f6849facb6645ca3c9addc1f036f8
Erick <Erick Valderrama>
2024-01-31 19:45:32 -0500

    Operator MS

---
+++ operator/src/main/java/com/unir/operator/model/pojo/BorrowDto.java
@@ -0,0 +1,28 @@
+package com.unir.operator.model.pojo;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class BorrowDto {
+	private Long bookId;
+	private Long personId;
+	@JsonDeserialize( using = LocalDateTimeDeserializer.class )
+	@JsonSerialize( using = LocalDateTimeSerializer.class )
+	private LocalDateTime date;
+	private Integer days;
+	@JsonDeserialize( using = LocalDateTimeDeserializer.class )
+	@JsonSerialize( using = LocalDateTimeSerializer.class )
+	private LocalDateTime returnDate;
+}
